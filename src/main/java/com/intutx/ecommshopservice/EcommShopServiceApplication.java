c9ee2e4fda04a40e817149a96b6f12f7d37ab3e0
koder me <koder.me@gmail.com>
2018-06-19 17:03:08 +0530

    Initial checkin

---
+++ src/main/java/com/intutx/ecommshopservice/EcommShopServiceApplication.java
@@ -0,0 +1,12 @@
+package com.intutx.ecommshopservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommShopServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommShopServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/intutx/ecommshopservice/EcommShopServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.intutx.ecommshopservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EcommShopServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
